33a44e482a75add6781b1cfe3d9b700286b9795a
aishukr18 <aishukrgowda2003@gmail.com>
2025-06-08 11:05:35 +0530

    Create clk-divider_tb.v

---
+++ day72/clk-divider_tb.v
@@ -0,0 +1,20 @@
+module divider_tb;
+    reg clk,reset;
+    wire derv_clk;
+    
+    divider dut(clk,reset,derv_clk);
+    initial
+        begin
+            clk = 0;
+            forever
+            #2 clk = ~clk;
+         end 
+    initial
+        begin
+            reset = 1;
+            @(negedge clk);
+            reset = 0;
+            #100;
+            $finish;
+        end
+endmodule 
